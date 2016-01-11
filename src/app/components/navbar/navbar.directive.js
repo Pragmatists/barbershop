227660480aff0774344ad5b42eb5fe0598efecbc
Michael Bareja <michal.bareja@pragmatists.pl>
2016-01-11 09:17:01 +0100

    cleaned up generated stuff

--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -1,32 +1,16 @@
-(function() {
+(function () {
   'use strict';
 
   angular
     .module('barbershop')
-    .directive('acmeNavbar', acmeNavbar);
+    .directive('acmeNavbar', function acmeNavbar() {
+        return {
+          restrict: 'E',
+          templateUrl: 'app/components/navbar/navbar.html'
+        };
 
-  /** @ngInject */
-  function acmeNavbar() {
-    var directive = {
-      restrict: 'E',
-      templateUrl: 'app/components/navbar/navbar.html',
-      scope: {
-          creationDate: '='
-      },
-      controller: NavbarController,
-      controllerAs: 'vm',
-      bindToController: true
-    };
+      }
+    );
 
-    return directive;
-
-    /** @ngInject */
-    function NavbarController(moment) {
-      var vm = this;
-
-      // "vm.creation" is avaible by directive option "bindToController: true"
-      vm.relativeDate = moment(vm.creationDate).fromNow();
-    }
-  }
 
 })();
