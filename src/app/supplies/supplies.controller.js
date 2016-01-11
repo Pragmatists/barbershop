651bd0d51599bab26138317d8aead839e7ee8889
Michael Bareja <michal.bareja@pragmatists.pl>
2016-01-11 12:32:29 +0100

    decrease supply amount

--- src/app/supplies/supplies.controller.js
+++ src/app/supplies/supplies.controller.js
@@ -8,4 +8,10 @@ angular.module('barbershop.supplies')
       suppliesService.store(vm.new.item, vm.new.amount);
       delete vm.new;
     };
+
+    vm.decrease = function (item) {
+      if (item.amount) {
+        suppliesService.take(item.item, 1);
+      }
+    }
   });
