3e4f48a1f594ffa020472b6b9cc913cec1109dc6
Michael Bareja <michal.bareja@pragmatists.pl>
2016-07-14 16:08:30 +0200

    tests for phoneNumber filter

---
+++ src/app/appointments/appointment/phoneNumber.filter.spec.js
@@ -0,0 +1,19 @@
+describe('phoneNumber filter', function () {
+
+    var phoneNumberFilter;
+
+    beforeEach(module('barbershop.appointments'));
+
+    beforeEach(inject(function (_phoneNumberFilter_) {
+        phoneNumberFilter = _phoneNumberFilter_;
+    }));
+
+    xit('filters', function () {
+        expect(phoneNumberFilter(undefined)).toBe(undefined);
+        expect(phoneNumberFilter('123456789')).toBe('123 456 789');
+        expect(phoneNumberFilter('1234')).toBe('1 234');
+        expect(phoneNumberFilter('123')).toBe('123');
+    });
+
+
+});
