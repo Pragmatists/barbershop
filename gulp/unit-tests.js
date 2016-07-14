2f84cb9de16a946193db42318157e6ab7663420a
Michael Bareja <michal.bareja@pragmatists.pl>
2016-07-14 11:50:41 +0200

    newer angular

--- gulp/unit-tests.js
+++ gulp/unit-tests.js
@@ -6,35 +6,15 @@ var conf = require('./conf');
 
 var karma = require('karma');
 
-var pathSrcHtml = [
-  path.join(conf.paths.src, '/**/*.html')
-];
-
-var pathSrcJs = [
-  path.join(conf.paths.src, '/**/!(*.spec).js')
-];
-
 function runTests (singleRun, done) {
   var reporters = ['progress'];
   var preprocessors = {};
-
-  pathSrcHtml.forEach(function(path) {
-    preprocessors[path] = ['ng-html2js'];
-  });
-
-  if (singleRun) {
-    pathSrcJs.forEach(function(path) {
-      preprocessors[path] = ['coverage'];
-    });
-    reporters.push('coverage')
-  }
 
   var localConfig = {
     configFile: path.join(__dirname, '/../karma.conf.js'),
     singleRun: singleRun,
     autoWatch: !singleRun,
-    reporters: reporters,
-    preprocessors: preprocessors
+      reporters : reporters
   };
 
   var server = new karma.Server(localConfig, function(failCount) {
