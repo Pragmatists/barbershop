1c6c6c858436b7fa6658171c6ea2352a5e7df1cf
Michael Bareja <michal.bareja@pragmatists.pl>
2016-07-14 11:53:14 +0200

    separate module for karma template cache

--- karma.conf.js
+++ karma.conf.js
@@ -50,7 +50,7 @@ module.exports = function(config) {
 
         ngHtml2JsPreprocessor : {
             stripPrefix : conf.paths.src + '/',
-            moduleName : 'barbershop'
+            moduleName : 'barbershop.templates'
         },
 
         logLevel : 'WARN',
